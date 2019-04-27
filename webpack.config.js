aaf1c44f90742c08728f2fb98a10bb6f7a58e40c
seon <seonu0823@gmail.com>
2019-04-27 15:23:08 +0900

    개발 환경 세팅

---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
+const PORT = process.env.PORT || 3000;
+
+const config = {
+    mode : 'development',
+    entry : path.join(__dirname, '/src/index.tsx'),
+    output : {
+        filename : 'bundle.[hash].js',
+        path : path.join(__dirname, '/dist')
+    },
+    resolve : {
+        extensions : ['.js', '.json', '.ts', '.tsx']
+    },
+    devtool : 'inline-source-map',
+    module : {
+        rules : [
+            {
+                test : /\.(ts|tsx)$/,
+                exclude : /node_modules/,
+                use : ['ts-loader']
+            },
+            {
+                test : /\.(scss|css)$/,
+                use : [
+                    {
+                        loader : 'style-loader'
+                    },
+                    {
+                        loader : 'css-loader',
+                        options : {
+                            modules : true,
+                            camelCase : true,
+                            sourceMap : true,
+                            localIdentName : '[local]__[hash:base64:5]'
+                        }
+                    },
+                    {
+                        loader : 'sass-loader',
+                        options : {
+                            sourceMap : true,
+                            data : `@import './src/globalStyles';`
+                        }
+                    }
+                ]
+            },
+            {
+                test : /\.(jpg|png|svg|ico|icns)$/,
+                loader : 'file-loader',
+                options : {
+                    name : '[path][name].[ext]'
+                }
+            }
+        ]
+    },
+    plugins : [
+        new HtmlWebpackPlugin({
+            template : path.resolve(__dirname, './public/index.html')
+        }),
+        new Dotenv()
+    ],
+    devServer : {
+        host : 'localhost',
+        port : PORT,
+        historyApiFallback : true,
+        open : true
+    }
+};
+
+module.exports = config;
