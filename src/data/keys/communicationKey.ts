abc0cd296a5ccb1a14fe1ea3c6d96c8cdf410cee
seon <seonu0823@gmail.com>
2019-05-08 21:53:16 +0900

    의사소통 중간 커밋

--- src/data/keys/communicationKey.ts
+++ src/data/keys/communicationKey.ts
@@ -1,3 +1,14 @@
-const COMMUNICATION_KEY = {};
+const CONTENT_CATEGORY_KEYS = {
+
+};
+
+const DETAIL_KEYS = {
+
+};
+
+const COMMUNICATION_KEY = {
+    CONTENT_CATEGORY_KEYS,
+    
+};
 
 export default COMMUNICATION_KEY;
