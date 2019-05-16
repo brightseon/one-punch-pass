a081c29d77d797fbe02f9bafa77b799760e5ba6c
seon <seonu0823@gmail.com>
2019-05-16 23:39:18 +0900

    필요없는 파일 삭제

--- src/data/sections/index.ts
+++ src/data/sections/index.ts
@@ -1,17 +1,7 @@
 import GOSIMOOM_SECTION from './gosimoonSection';
 import SUMMARY_SECTION from './summarySection';
-import PHYSICAL_EXERCISE_HEALTH_SECTION from './physicalExerciseHealthSection';
-import COMMUNICATION_SECTION from './communicationSection';
-import SOCIAL_RELATIONS_SECTION from './socialRelationsSection';
-import ART_EXPERIENCE_SECTION from './artExperienceSection';
-import NATURE_INQUIRY_SECTION from './natureInquirySection';
 
 export default {
     GOSIMOOM_SECTION,
-    SUMMARY_SECTION,
-    PHYSICAL_EXERCISE_HEALTH_SECTION,
-    COMMUNICATION_SECTION,
-    SOCIAL_RELATIONS_SECTION,
-    ART_EXPERIENCE_SECTION,
-    NATURE_INQUIRY_SECTION
+    SUMMARY_SECTION
 };
